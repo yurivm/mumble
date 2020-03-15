5bc8c4cb455f8800d969078d5754ef41b13fdea9
Yuri Veremeyenko <yuri.veremeyenko@gmail.com>
2020-03-15 19:36:40 +0100

    again

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const net = require('net');
  * Create TCP server that receives data and sends it to clients in a loop
  */
 
+let servers = [];
+
 exports.startTCPServer = (port, clients) => {
   function handlePotConnection(conn) {
     const remoteAddress = `${conn.remoteAddress}:${conn.remotePort}`;
@@ -44,6 +46,7 @@ exports.startTCPServer = (port, clients) => {
 
   const server = net.createServer();
   server.on('connection', handlePotConnection);
+  servers.push(server);
 
   server.listen(port, () => {
     console.log('server listening to %j', server.address());
